7914753d331a66e3e931b157e9c9bb6cc481bf36
PiotrMor <piotrmorawiecki97@gmail.com>
2020-11-06 11:26:33 +0100

    Add borders for cheapest and most expensive trips

--- src/app/components/trips/trips.component.ts
+++ src/app/components/trips/trips.component.ts
@@ -37,7 +37,7 @@ export class TripsComponent implements OnInit {
     return trip.availablePlaces == 0;
   }
 
-/*   isCheapest(trip: Trip): boolean {
+  isCheapest(trip: Trip): boolean {
     for (var element of this.tripList) {
       if (trip.price > element.price) {
         return false;
@@ -46,14 +46,14 @@ export class TripsComponent implements OnInit {
     return true;
   }
 
-  isMostExpansive(trip: Trip): boolean {
+  isMostExpensive(trip: Trip): boolean {
     for (var element of this.tripList) {
       if (trip.price < element.price) {
         return false;
       }
     }
     return true;
-  } */
+  }
 
   addRandomTrips() {
     let trip1: Trip = new Trip("Wycieczka do Paryża", "Francja");
@@ -69,55 +69,55 @@ export class TripsComponent implements OnInit {
     trip2.endDate = new Date("2021-01-5");
     trip2.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut condimentum velit, vitae posuere dolor. Morbi sit amet ante et nisl faucibus gravida non vel leo. Aenean non congue ante, eu malesuada tortor. Fusce faucibus, erat ut suscipit tempor, metus libero tristique sapien, ut malesuada tortor mi sed eros. Vivamus eget ligula neque. ";
     trip2.price = 704;
-    trip2.imageUrl = "https://lh3.googleusercontent.com/proxy/SKZ9NaLSaHvpZUDBXCBpISDgHyaAyEvtMNTvwx3vrkqaxkC-xXdL3yS4lMwGqnwJNlggGRm6mFdzTT27YBZd32P_iQahwcOtq9PHU4p0ckN2B_8dG6KVI_qaVxwc";
+    trip2.imageUrl = "https://cdn.pixabay.com/photo/2016/01/19/19/26/amsterdam-1150319_960_720.jpg";
     trip2.totalPlaces = 20;
 
-    let trip3: Trip = new Trip("Wycieczka do Paryża", "Francja");
+    let trip3: Trip = new Trip("Wycieczka do Moskwy", "Rosja");
     trip3.startDate = new Date("2020-12-28");
     trip3.endDate = new Date("2021-01-5");
     trip3.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut condimentum velit, vitae posuere dolor. Morbi sit amet ante et nisl faucibus gravida non vel leo. Aenean non congue ante, eu malesuada tortor. Fusce faucibus, erat ut suscipit tempor, metus libero tristique sapien, ut malesuada tortor mi sed eros. Vivamus eget ligula neque. ";
     trip3.price = 1204;
-    trip3.imageUrl = "https://www.travelmoney.co.nz/sites/v1.travelmoney.co.nz/files/France-small-640x480_1.jpg";
+    trip3.imageUrl = "https://cdn.pixabay.com/photo/2016/07/30/08/13/moscow-1556561_960_720.jpg";
     trip3.totalPlaces = 10;
 
-    let trip4: Trip = new Trip("Wycieczka do Paryża", "Francja");
+    let trip4: Trip = new Trip("Wycieczka do Barcelony", "Hiszpania");
     trip4.startDate = new Date("2020-12-28");
     trip4.endDate = new Date("2021-01-5");
     trip4.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut condimentum velit, vitae posuere dolor. Morbi sit amet ante et nisl faucibus gravida non vel leo. Aenean non congue ante, eu malesuada tortor. Fusce faucibus, erat ut suscipit tempor, metus libero tristique sapien, ut malesuada tortor mi sed eros. Vivamus eget ligula neque. ";
     trip4.price = 2204;
-    trip4.imageUrl = "https://www.travelmoney.co.nz/sites/v1.travelmoney.co.nz/files/France-small-640x480_1.jpg";
+    trip4.imageUrl = "https://cdn.pixabay.com/photo/2014/11/30/20/46/sagrada-familia-552084_960_720.jpg";
     trip4.totalPlaces = 20;
 
-    let trip5: Trip = new Trip("Wycieczka do Paryża", "Francja");
+    let trip5: Trip = new Trip("Wycieczka do Krakowa", "Polska");
     trip5.startDate = new Date("2020-12-28");
     trip5.endDate = new Date("2021-01-5");
     trip5.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut condimentum velit, vitae posuere dolor. Morbi sit amet ante et nisl faucibus gravida non vel leo. Aenean non congue ante, eu malesuada tortor. Fusce faucibus, erat ut suscipit tempor, metus libero tristique sapien, ut malesuada tortor mi sed eros. Vivamus eget ligula neque. ";
     trip5.price = 3204;
-    trip5.imageUrl = "https://www.travelmoney.co.nz/sites/v1.travelmoney.co.nz/files/France-small-640x480_1.jpg";
+    trip5.imageUrl = "https://cdn.pixabay.com/photo/2019/09/12/18/34/street-4472321_960_720.jpg";
     trip5.totalPlaces = 20;
 
-    let trip6: Trip = new Trip("Wycieczka do Paryża", "Francja");
+    let trip6: Trip = new Trip("Wycieczka do Berlina", "Niemcy");
     trip6.startDate = new Date("2020-12-28");
     trip6.endDate = new Date("2021-01-5");
     trip6.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut condimentum velit, vitae posuere dolor. Morbi sit amet ante et nisl faucibus gravida non vel leo. Aenean non congue ante, eu malesuada tortor. Fusce faucibus, erat ut suscipit tempor, metus libero tristique sapien, ut malesuada tortor mi sed eros. Vivamus eget ligula neque. ";
     trip6.price = 504;
-    trip6.imageUrl = "https://www.travelmoney.co.nz/sites/v1.travelmoney.co.nz/files/France-small-640x480_1.jpg";
+    trip6.imageUrl = "https://cdn.pixabay.com/photo/2013/10/28/18/51/brandenburger-tor-201939_960_720.jpg";
     trip6.totalPlaces = 20;
 
-    let trip7: Trip = new Trip("Wycieczka do Paryża", "Francja");
+    let trip7: Trip = new Trip("Wycieczka do Kopenhagi", "Dania");
     trip7.startDate = new Date("2020-12-28");
     trip7.endDate = new Date("2021-01-5");
     trip7.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut condimentum velit, vitae posuere dolor. Morbi sit amet ante et nisl faucibus gravida non vel leo. Aenean non congue ante, eu malesuada tortor. Fusce faucibus, erat ut suscipit tempor, metus libero tristique sapien, ut malesuada tortor mi sed eros. Vivamus eget ligula neque. ";
     trip7.price = 647;
-    trip7.imageUrl = "https://www.travelmoney.co.nz/sites/v1.travelmoney.co.nz/files/France-small-640x480_1.jpg";
+    trip7.imageUrl = "https://cdn.pixabay.com/photo/2020/01/06/21/24/evening-4746326_960_720.jpg";
     trip7.totalPlaces = 15;
 
-    let trip8: Trip = new Trip("Wycieczka do Paryża", "Francja");
+    let trip8: Trip = new Trip("Wycieczka do Los Angeles", "Stany Zjednoczone");
     trip8.startDate = new Date("2020-12-28");
     trip8.endDate = new Date("2021-01-5");
     trip8.description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut condimentum velit, vitae posuere dolor. Morbi sit amet ante et nisl faucibus gravida non vel leo. Aenean non congue ante, eu malesuada tortor. Fusce faucibus, erat ut suscipit tempor, metus libero tristique sapien, ut malesuada tortor mi sed eros. Vivamus eget ligula neque. ";
     trip8.price = 1204;
-    trip8.imageUrl = "https://www.travelmoney.co.nz/sites/v1.travelmoney.co.nz/files/France-small-640x480_1.jpg";
+    trip8.imageUrl = "https://cdn.pixabay.com/photo/2016/08/16/17/32/hollywood-sign-1598473__340.jpg";
     trip8.totalPlaces = 20;
     
     this.tripList.push(trip1);
