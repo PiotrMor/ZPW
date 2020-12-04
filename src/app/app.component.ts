b71577d91c7c733098eae729505bf69a908a7d50
PiotrMor <piotrmorawiecki97@gmail.com>
2020-12-04 04:20:38 +0100

    Improve navigation menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { AdminGuard } from './guards/admin.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ZPW';
+  isLogged: boolean = false;
+  isAdmin: boolean = false;
 
+  constructor(private authService: AuthService, private authGuard: AuthGuard, private adminGuard: AdminGuard) {
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn();
+    this.hasAdminRole();
+  }
+
+  logout() {
+    this.authService.logout().then(succ => console.log("Wylogowano!"));
+  }
+
+  isLoggedIn() {
+    this.authGuard.canActivate(null, null).subscribe(result => {
+      this.isLogged = result;
+    });
+  }
+
+  hasAdminRole() {
+    this.adminGuard.canActivate(null, null).subscribe(result => {
+      this.isAdmin = result;
+    });
+  }
 }
